54566852434cbda10eb3572944119a53db8d5224
amanjot-garcha-au19 <85173918+amanjot-garcha-au19@users.noreply.github.com>
2022-06-05 11:19:56 -0600

    Heroku Deployment

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get('/', (req,res) => {
-    res.sendFile(`${__dirname}/views/index.html`)
-});
+const fs = require('fs');
+
+const bodyparser = express.urlencoded({extended:false})
+
+const port = process.env.PORT || 3001;
+
+app.get('/' , (req,res) => {
+    res.sendFile(`${__dirname}/views/bookForm.html`);
+})
+
+app.post("/addBook" , bodyparser,(req,res) => {
+   const {bookname,price,isbn,author,publisher,date,edition} = req.body;
+    console.log(bookname,price,isbn,author,publisher,date,edition);
+    let books = fs.readFileSync('books.json' , (err,data) => {
+        if (err) console.log(err)
+        else return data;
+    } );
+    let Book = JSON.parse(books);
+    
+    let newBook = {bookName : bookname,price:price,ISBN:isbn,authorName:author,publisherName:publisher,publishedDate:date,edition:edition}
+    Book.push(newBook);
+    let book = JSON.stringify(Book)
+    fs.writeFileSync('books.json' ,book )
+    res.send('book added')
+})
 
 
-const Port = process.env.PORT  || 3000
-app.listen(Port);
+app.listen(port, console.log('App is live on :', port))
